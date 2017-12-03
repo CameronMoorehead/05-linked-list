3829f42f6dd3e5d999f0062f4aeb193bd59ea6a2
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-03 14:35:54 -0800

    added code from class

---
+++ lab-cameron/model/linked-list.js
@@ -0,0 +1,44 @@
+'use strict';
+
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+  append(node) {
+    if (!(node instanceof LinkedList)) {
+      throw new TypeError('<node> should be an instance of LinkedList');
+    }
+
+    if (!this.next) {
+      this.next = node;
+    } else {
+      this.next.append(node);
+    }
+
+    return this;
+  }
+
+  find(value) {
+    console.log(value);
+  }
+
+  remove(node) {
+    if (!(node instanceof LinkedList)) {
+      throw new TypeError('<node> should be an instance of LinkedList');
+    }
+
+    if (!this.next) {
+      return this;
+    }
+    if (this.next === node) {
+      this.next = this.next.next;
+    } else {
+      this.next.remove(node);
+    }
+    return this;
+  }
+}
+
+module.exports = LinkedList;
