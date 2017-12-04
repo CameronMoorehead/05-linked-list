ccc4c7e352617e2cca3b1fdc9c498e2105f08002
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-03 16:31:58 -0800

    restructured tests

--- lab-cameron/__test__/linked-list.test.js
+++ lab-cameron/__test__/linked-list.test.js
@@ -3,48 +3,52 @@
 const LinkedList = require('../model/linked-list');
 
 describe('linked-list.js', () => {
-  test('A list with a single element, should have a value and no next', () => {
-    const result = new LinkedList(5);
-    expect(result.value).toEqual(5);
-    expect(result.next).toBeNull();
+  describe('append method', () => {
+    test('A list with a single element, should have a value and no next', () => {
+      const result = new LinkedList(5);
+      expect(result.value).toEqual(5);
+      expect(result.next).toBeNull();
+    });
+
+    test('append should properly modify the next property', () => {
+      const result = new LinkedList(5);
+      result.append(new LinkedList(4));
+      result.append(new LinkedList(10));
+
+      expect(result.value).toEqual(5);
+      expect(result.next.value).toEqual(4);
+      expect(result.next.next.value).toEqual(10);
+      expect(result.next.next.next).toBeNull();
+    });
   });
 
-  test('append should properly modift the next property', () => {
-    const result = new LinkedList(5);
-    result.append(new LinkedList(4));
-    result.append(new LinkedList(10));
+  describe('remove method', () => {
+    test('remove should update the next property and erase an element', () => {
+      const first = new LinkedList(10);
+      const second = new LinkedList(20);
+      const third = new LinkedList(30);
 
-    expect(result.value).toEqual(5);
-    expect(result.next.value).toEqual(4);
-    expect(result.next.next.value).toEqual(10);
-    expect(result.next.next.next).toBeNull();
-  });
+      first.append(second);
+      second.append(third);
 
-  test('remove should update the next property and erase an element', () => {
-    const first = new LinkedList(10);
-    const second = new LinkedList(20);
-    const third = new LinkedList(30);
+      expect(first.value).toEqual(10);
+      expect(first.next.value).toEqual(20);
+      expect(first.next.next.value).toEqual(30);
+      expect(first.next.next.next).toBeNull();
 
-    first.append(second);
-    second.append(third);
+      first.remove(second);
 
-    expect(first.value).toEqual(10);
-    expect(first.next.value).toEqual(20);
-    expect(first.next.next.value).toEqual(30);
-    expect(first.next.next.next).toBeNull();
+      expect(first.value).toEqual(10);
+      expect(first.next.value).toEqual(30);
+      expect(first.next.next).toBeNull();
+    });
 
-    first.remove(second);
+    test('should return null if linked list is one element and head is removed', () => {
+      const first = new LinkedList(10);
 
-    expect(first.value).toEqual(10);
-    expect(first.next.value).toEqual(30);
-    expect(first.next.next).toBeNull();
-  });
+      first.remove(first);
 
-  test('should return null if linked list is one element and head is removed', () => {
-    const first = new LinkedList(10);
-
-    first.remove(first);
-
-    expect(first.value).toBeNull();
+      expect(first.value).toBeNull();
+    });
   });
 });
